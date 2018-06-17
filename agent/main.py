29239435e83380a392baba490f0ea4f5cb220f79
Neo Tran <neo_tran@outlook.com>
2018-06-17 17:25:41 -0500

    Comentarios and main function

--- agent/main.py
+++ agent/main.py
@@ -1,16 +1,16 @@
 import requests
 import pymongo
 
-API_URL = 'https://api.coinmarketcap.com/v2/ticker/' # API address
+API_URL = 'https://api.coinmarketcap.com/v1/ticker/' # API address
 
-def get_db_conection(uri): # Varible uri = Mongo server address
-    client = pymongo.MongoClient(uri) # Create Client in MongoDB
+def get_db_connection(url): # Varible url = Mongo server address
+    client = pymongo.MongoClient(url) # Create Client in MongoDB
     return client.cryptongo
 
 def get_cryptocurrencies_from_api():
     r = requests.get(API_URL) # Get API result
 
-    if r.statud_code == 200: # Verify if the code state is the one that is needed
+    if r.status_code == 200: # Verify if the code state is the one that is needed
         result = r.json() # Convert information to Json
         return result
     
@@ -18,48 +18,50 @@ def get_cryptocurrencies_from_api():
 
 def get_hash(value):
     from hashlib import sha512
-    return sha512(value.encode('utf-8')).hexdigest()
+    return sha512(value.encode('utf-8')).hexdigest() # Hexdigest = Convert to str
 
 def first_element(elements):
-    return elements [0]
+    return elements[0]
 
 def get_ticker_hash(ticker_data):
-    from collections import OrdereDict
-    ticker_data = OrdereDict(
-        sorted(
-            ticker_data.items(),
-            key = first_element
-
-            )
-        )
-
+    from collections import OrderedDict # Library to bring a function that will create an ordered dictionary
+    ticker_data = OrderedDict(sorted(ticker_data.items(), key = first_element)) # sorted = Sort a data structure
     ticker_value = ''
 
     for _, value in ticker_data.items():
-        ticker_value += str(value)
+        ticker_value += str(value) # Save value in the variable
 
     return get_hash(ticker_value)
+    
 
-def check_if_exists(db_coneccion, ticker_data):
+def check_if_exists(db_connection, ticker_data):
     ticker_hash = get_ticker_hash(ticker_data)
-    if db_coneccion.tickers.find_one({'ticker_hash': ticker_hash}): # If exist into Database, return True
+
+    if db_connection.tickers.find_one({'ticker_hash': ticker_hash}): # If exist into Database, return True
         return True
 
     return False
 
-def save_ticker(db_coneccion, ticker_data = None):
+def save_ticker(db_connection, ticker_data = None):
     if not ticker_data:
         return False
     
-    if check_if_exists(db_coneccion, ticker_data): # If the function return true, then cancel process
+    if check_if_exists(db_connection, ticker_data): # If the function return true, then cancel process
         return False
 
     ticker_hash = get_ticker_hash(ticker_data)
-    ticker_data['ticker_hash'] = ticker_hash
+    ticker_data['ticker_hash'] = ticker_hash # Get the dictionary key
     ticker_data['rank'] = int(ticker_data['rank']) # Get values and Convert to int
     ticker_data['last_updated'] = int(ticker_data['last_updated'])
 
-    db_coneccion.tickers.insert_one(ticker_data) # Insert all ticket information
+    db_connection.tickers.insert_one(ticker_data) # Insert all ticket information
     return True
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    connection = get_db_connection('mongodb://localhost:27017/') # (Connection protocol URL://IP:Port/)
+    tickers = get_cryptocurrencies_from_api()
+
+    for ticker in tickers:
+        save_ticker(connection, ticker) # Store the ticker in the cycle
+
+    print("Stored tickers")
--- api/main.py
+++ api/main.py
@@ -1,44 +1,50 @@
 import pymongo
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request # jsonify returns in json format
+# Flask is to raise a web server
+def get_db_connection(url): # Varible url = Mongo server address
+    client = pymongo.MongoClient(url) # Create Client in MongoDB
+    return client.cryptongo
 
-def get_db_connection(uri):
-    client = pymongo.MongoClient(uri)
-    return  client.cryptongo
+app = Flask(__name__) # Create a web server
+db_connection = get_db_connection('mongodb://localhost:27017/') # (Connection protocol URL://IP:Port/)
 
-app = Flask(__name__)
-db_connection = get_db_connection('mongodb://localhost:27017/')
+def get_document_and_top20(top = False): # Get the list of documents to return them
+    params = {} # Data dictionary
+    name = request.args.get('name', '') # Get name from GET, if it doesn't exist, then null
+    limit = int(request.args.get('limit', 0)) # Get limit from method GET, if it dowsn't exist, then limit equals 0
 
-def get_documents():
-	params ={}
-	name = int(request.args.get('name',''))
-	limit = int(request.args.get('limit', 0))
-	if name:
-		params.udpate({'name':name})
-	cursor=db_connection.tickers.find(
-		params,{'_id':0,'ticker_hash':0}).limit(limit)
-	return list(cursor)
+    if name: params.update({'name': name}) # If the name exist, then add data dictionary
+    if top: params.update({'rank': {'$lte': 20}}) # Show the first 20 in the ranking
 
-def get_top20():
-	params = {}
-	name = int(request.args.get('name', ''))
-	limit = int(request.args.get('limit', 0))
-	if name:
-		params.udpate({'name':name})
-        params.update({'rank': {'$lte': 20}})
-	cursor = db_connection.tickers.find(
-		params, {'_id': 0, 'ticker_hash': 0}
-        ).limit(limit)
-	return list(cursor)
+    cursor = db_connection.tickers.find(params, {'_id': 0, 'ticker_hash': 0}).limit(limit)
+    # Search for tickets as received from the GET method
+    return list(cursor)
 
-	
+def remove_currency(): # Deleted Data
+    params = {}
+    name = request.args.get('name', name)
+    
+    if name: params.update({'name': name})
+    else: return False
+    # Delete all data that matches what the method received
+    return db_connection.tickers.delete_many(params).deleted_count 
+    
+@app.route("/") # Define the route
+def index():
+    return jsonify({ 'name': 'Cryptongo API' })
 
-def remove_currency():
-	params = {}
-    name = request.args.get('name', '')
-    if name:
-        params.update({'name': name})
-    else:
-        return False
-    return db_connection.tickers.delete_many(
-        params
-    ).deleted_count
+@app.route("/tickers", methods=['GET', 'DELETE'])
+def tickers():
+    if request.method == 'GET': # If the method is GET, then execute this function
+        return jsonify(get_document_and_top20())
+    elif request.method == 'DELETE':
+        result = remove_currency() # Else if the method is DELETE, then execute this function
+        
+        if result > 0:
+            return jsonify({'text': 'Deleted document'}), 204 # FLASK allows you to add another parameter to the return
+        else:
+            return jsonify({'error': 'The documents do not exist'}), 404
+            
+@app.route("/top20", methods=['GET'])
+def top20():
+    return jsonify(get_document_and_top20(True))
